a154f5c0a8488bed87fd32f3ea8e71aa326d1c3b
Amit Diggavi <diggavi@wisc.edu>
2022-12-11 13:53:50 -0800

    some changes to those files

---
+++ src/DataWranglerTests.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DataWranglerTests {
+    GraphCreator<ILocation, Double> graph;
+
+    @BeforeEach
+    public void createGraph() {
+        this.graph = new GraphCreator<>();
+    }
+
+
+    /**
+     * testing the getLocation method
+     */
+    @Test
+    public void test1() {
+        Location location = new Location("Madison");
+
+        assertEquals("Madison", location.getLocation());
+    }
+
+    /**
+     * testing if the method loads the correct amount of locations
+     */
+    @Test
+    public void test2() {
+        try {
+            new LocationLoader(graph).loadLocations("p3dot.dot");
+
+        } catch (Exception e) {
+
+        }
+
+        assertEquals(21, graph.getVertexCount());
+    }
+
+    /**
+     * tests if the method throws an expection if file is not found
+     */
+    @Test
+    public void test3() {
+        boolean bool = false;
+        try {
+            new LocationLoader(graph).loadLocations("p3.dot");
+        } catch (Exception exception) {
+            bool = true;
+        }
+        assertTrue(bool);
+    }
+
+    /**
+     * test if the amount of edges is correct
+     */
+    @Test
+    public void test4() {
+        {
+            try {
+                new LocationLoader(graph).loadLocations("p3dot.dot");
+
+            } catch (Exception e) {
+
+            }
+            assertEquals(75, graph.getEdgeCount());
+        }
+    }
+
+    /**
+     * check for specific edges and if they match
+     */
+    @Test
+    public void test5()
+    {
+        try {
+            new LocationLoader(graph).loadLocations("p3dot.dot");
+
+        } catch (Exception e) {
+
+        }
+        assertTrue(graph.containsEdge(new Location("Chicago"), new Location("Milwaukee")));
+    }
+}
---
+++ src/LocationLoader.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class LocationLoader implements ILocationLoader {
+    IGraph<ILocation, Double> graph;
+
+    public LocationLoader(IGraph<ILocation, Double> graph)
+    {
+        this.graph = graph;
+    }
+    /**
+     * This method loads the list of locations from a DOT file.
+     * @param filePathToDOT path to the DOT file
+     */
+    @Override
+    public void loadLocations(String filePathToDOT) throws FileNotFoundException {
+
+
+        Scanner scan = new Scanner(new File(filePathToDOT));
+
+
+            ArrayList<ArrayList<String>> locations = new ArrayList<>();
+            LinkedHashSet<String> lhs = new LinkedHashSet<>();
+            while (scan.hasNextLine()) {
+                String str = scan.nextLine();
+                if (str.contains("->")) {
+
+                    String[] b = str.split(" -> ");
+                    ArrayList<String> str_list = new ArrayList<>();
+                    String from = b[0].trim();
+                    str_list.add(from);
+                    String to = b[1].substring(0, b[1].indexOf("[")).trim();
+                    str_list.add(to);
+                    String weight = b[1].substring(b[1].indexOf("=") + 1).trim().replaceAll("[^\\d.]", "");// replace all non digits
+                    str_list.add(weight);
+                    locations.add(str_list);
+
+                }
+            }
+
+            for (ArrayList<String> str : locations) {
+                for (int i = 0; i < 2; i++) {
+                    lhs.add((str.get(i)));
+                }
+            }
+
+            for (String location : lhs) {
+                graph.insertVertex(new Location(location));
+            }
+            for (ArrayList<String> location : locations) {
+                ILocation from_location = (new Location(location.get(0)));
+                ILocation to_location = (new Location(location.get(1)));
+                double convert_to_double = (Double.parseDouble(location.get(2)));
+                graph.insertEdge(from_location, to_location, convert_to_double);
+            }
+    }
+
+}

d13c0f01f56ed031084bcffdf26e95356f20c203
Matthew Moy <mgmoy@wisc.edu>
2022-12-13 20:51:34 -0600

    integration and partner tests

--- AlgorithmEngineerTests.java
+++ AlgorithmEngineerTests.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -80,5 +82,192 @@ public class AlgorithmEngineerTests {
 
     public ILocation makeLoc(String name) {
         return new Location(name);
+    }
+
+    /**
+     * Uses the backend method calculateRoute which uses the shortestPath method
+     * to find the route given two locations in a graph
+     */
+    @Test
+    public void integrationTestCalculateRoute() {
+        Backend backend = new Backend();
+        ArrayList<ILocation> path = new ArrayList<>();
+        String expectedRoute = "Madison,Middleton,La Crosse,Rochester";
+        backend.setCurrentLocation("Madison");
+        backend.setTargetLocation("Rochester");
+        path = backend.calculateRoute();
+        String calculatedRoute = "";
+
+        for(ILocation p: path)
+        {
+            calculatedRoute = calculatedRoute + (calculatedRoute.length() > 0? ",": "") +
+                p.getLocation();
+        }
+
+        assertEquals(expectedRoute, calculatedRoute);
+    }
+
+    /**
+     * Uses the backend method calculateRouteDistance which uses the shortestPathand getPathCost
+     * methods to find the route distance given two locations in a graph
+     */
+    @Test
+    public void integrationTestCalculateRouteDistance() {
+        Backend backend = new Backend();
+        backend.setCurrentLocation("Madison");
+        backend.setTargetLocation("Rochester");
+        ArrayList<String> stops = new ArrayList<>();
+        ArrayList<Double> distances = backend.calculateRouteDistance();
+        Double totalDistance = 0.0;
+
+        for (Double d : distances) {
+            totalDistance += d;
+        }
+
+        assertEquals(totalDistance, 215.0);
+    }
+
+    /**
+     * Tests the find route functionality.
+     */
+    @Test
+    public void codeReviewofFrontendDeveloperTest1() {
+        TextUITester uiTester = new TextUITester("2\nMadison\n3\nChicago\n5\n7\n");
+        Scanner scn = new Scanner(System.in);
+        IBackend backend = new Backend();
+        IFrontend frontend = new Frontend(backend, scn);
+        frontend.runCommandLoop();
+
+        String expected = """
+                Welcome to MadMapper
+                —--------------------------------------------------
+
+                You are in the Main Menu:
+                Current Itinerary: Unset -> Unset
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+
+                Enter City:
+
+                You are in the Main Menu:
+                Current Itinerary: Madison -> Unset
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+
+                Enter City:
+
+                You are in the Main Menu:
+                Current Itinerary: Madison -> Chicago
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+                
+                The shortest route is:
+                Route: Madison -> Rockford (75.0 Miles) -> Chicago (90.0 Miles)
+                Total Distance: 165.0 Miles
+                                
+                Safe travels!
+                                
+                You are in the Main Menu:
+                Current Itinerary: Madison -> Chicago
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+                            
+                """;
+
+        // Check that the output is correct
+        assertEquals(expected, uiTester.checkOutput());
+    }
+
+    /**
+     * Tests the reset route functionality.
+     */
+    @Test
+    public void codeReviewofFrontendDeveloperTest2() {
+        TextUITester uiTester = new TextUITester("2\nMadison\n3\nChicago\n6\n7\n");
+        Scanner scn = new Scanner(System.in);
+        IBackend backend = new Backend();
+        IFrontend frontend = new Frontend(backend, scn);
+        frontend.runCommandLoop();
+
+        String expected = """
+                Welcome to MadMapper
+                —--------------------------------------------------
+
+                You are in the Main Menu:
+                Current Itinerary: Unset -> Unset
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+
+                Enter City:
+
+                You are in the Main Menu:
+                Current Itinerary: Madison -> Unset
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+
+                Enter City:
+
+                You are in the Main Menu:
+                Current Itinerary: Madison -> Chicago
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+                
+                You are in the Main Menu:
+                Current Itinerary: Unset -> Unset
+                Choose an option:
+                \t 1. List all cities
+                \t 2. Set current city
+                \t 3. Set target city
+                \t 4. Add stop
+                \t 5. Find route
+                \t 6. Reset stops
+                \t 7. Exit
+                            
+                """;
+
+        // Check that the output is correct
+        assertEquals(expected, uiTester.checkOutput());
     }
 }
